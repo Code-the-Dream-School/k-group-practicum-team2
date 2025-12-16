cf5590db927092eb3442d7fedea150b0426db38e
abraham <abraham@abrahams-MacBook-Air.local>
2025-12-16 15:57:48 -0500

    Profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_16_154655) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_16_184634) do
+  create_table "profiles", force: :cascade do |t|
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_16_154655) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "profiles", "users"
 end
