135d0faa39424736261de5cda8a6e8802c7a5e2c
Brittany Halterman <halterma@gmail.com>
2025-12-30 18:33:48 -0500

    Generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "projects/index"
+  get "projects/new"
+  get "projects/create"
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
