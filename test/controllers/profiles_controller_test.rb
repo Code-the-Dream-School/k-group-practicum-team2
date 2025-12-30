aa9ce6d2bcbff565b497f8955c48542749a7b1c6
abraham <abraham@abrahams-MacBook-Air.local>
2025-12-30 13:39:52 -0500

    test controller, gem update

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,23 +1,30 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email: "test@example.com", password: "password", password_confirmation: "password")
+    @profile = @user.create_profile!(first_name: "Test", last_name: "User", bio: "Testing")
+
+    post user_session_path, params: { user: { email: @user.email, password: "password" } }
+  end
+
   test "should get index" do
-    get profiles_index_url
+    get user_profiles_path(@user)
     assert_response :success
   end
 
   test "should get new" do
-    get profiles_new_url
+    get new_user_profile_path(@user)
     assert_response :success
   end
 
   test "should get show" do
-    get profiles_show_url
+    get user_profile_path(@user, @profile)
     assert_response :success
   end
 
   test "should get edit" do
-    get profiles_edit_url
+    get edit_user_profile_path(@user, @profile)
     assert_response :success
   end
 end
