2907ca707e9fc17b68cee5eca2f2118513feb5e3
abraham <abraham@abrahams-MacBook-Air.local>
2025-12-30 11:09:47 -0500

    Active storage

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -48,7 +48,7 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :bio, :skill_ids => [])
+    params.require(:profile).permit(:first_name, :last_name, :bio, :avatar,:skill_ids => [])
   end
   def set_skills
     @skills = Skill.all
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,7 @@ class Profile < ApplicationRecord
 
   has_many :profile_skills
   has_many :skills, through: :profile_skills
+  has_one_attached :avatar
 
   validates :first_name, :last_name,  presence: true
 end
