17f4c9a580df63dd3a1cbd62e0e38c7501265f3e
Brittany Halterman <halterma@gmail.com>
2026-01-02 21:10:10 -0500

    Set up controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,27 @@
 class ProjectsController < ApplicationController
+  # Ensure user is logged in
+  before_action :authenticate_user!
+
   def index
+    @projects = Project.all
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = current_user.projects.new(project_params)
+    if @project.save
+      redirect_to projects_path, notice: 'New project was created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
