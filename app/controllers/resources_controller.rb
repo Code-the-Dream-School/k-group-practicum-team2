0e3f44bdfb3ade5680880d6b29dcb0bd2d342006
Brittany Halterman <halterma@gmail.com>
2026-01-06 16:02:27 -0500

    Implement resources index and create functionality

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,10 +1,26 @@
 class ResourcesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @resources = Resource.all
   end
 
   def new
+    @resource = Resource.new
   end
 
   def create
+    @resource = current_user.resources.new(resource_params)
+    if @resource.save
+      redirect_to resources_path, notice: 'New resource was posted!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:title, :description, :url)
   end
 end
