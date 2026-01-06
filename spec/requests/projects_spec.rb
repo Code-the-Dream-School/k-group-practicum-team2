6ae796a789d893320456037f3f013bab83642431
Brittany Halterman <halterma@gmail.com>
2026-01-05 21:02:30 -0500

    Add automated testing for projects show

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -42,4 +42,43 @@ RSpec.describe "Projects", type: :request do
       expect(response.body).to include('Test project description 2.')
     end
   end
+
+  describe 'GET /projects/:id', type: :request do
+    let!(:user) do
+      User.create!(
+        email: 'test@mail.com',
+        password: 'secret'
+      )
+    end
+
+    let!(:project) do
+      Project.create!(
+        title: 'Test Project',
+        description: 'Test project description.',
+        user: user
+      )
+    end
+
+    before do
+      sign_in user, scope: :user
+    end
+
+    it 'responds with 200 OK' do
+      get "/projects/#{project.id}"
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a page containing the project title' do
+      get "/projects/#{project.id}"
+
+      expect(response.body).to include('Test Project')
+    end
+
+    it 'returns a page containing the project description' do
+      get "/projects/#{project.id}"
+
+      expect(response.body).to include('Test project description')
+    end
+  end
 end
