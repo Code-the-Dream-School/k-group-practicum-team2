652cca904d29db213c13c73c054d2d29892d9a7b
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-06 11:56:43 -0500

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_30_155919) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_26_201252) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -38,8 +38,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_30_155919) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
-ActiveRecord::Schema[8.1].define(version: 2025_12_26_201252) do
   create_table "bookmarked_projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "project_id", null: false
