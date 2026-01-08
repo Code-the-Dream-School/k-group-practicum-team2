0c36119f0fbf0088a9ca7614fd1102769087ad9a
Brittany Halterman <halterma@gmail.com>
2026-01-08 12:43:18 -0500

    Add index view

--- spec/requests/resources_spec.rb
+++ spec/requests/resources_spec.rb
@@ -9,13 +9,42 @@ RSpec.describe "Resources", type: :request do
       )
     end
 
+    let!(:resource1) do
+      Resource.create!(
+        title: 'Test Resource 1',
+        description: 'Test resource description 1.',
+        url: 'https://example.com/resource1',
+        user: user
+      )
+    end
+
+    let!(:resource2) do
+      Resource.create!(
+        title: 'Test Resource 2',
+        description: 'Test resource description 2.',
+        url: 'https://example.com/resource2',
+        user: user
+      )
+    end
+
     before do
       sign_in user, scope: :user
     end
 
-    it "returns http success" do
-      get "/resources/index"
-      expect(response).to have_http_status(:success)
+    it "returns with 200 OK" do
+      get '/resources'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a page containing title, descriptions and urls of all the projects' do
+      get '/resources'
+
+      expect(response.body).to include('Test Resource 1')
+      expect(response.body).to include('Test resource description 1.')
+      expect(response.body).to include('https://example.com/resource1')
+      expect(response.body).to include('Test Resource 2')
+      expect(response.body).to include('Test resource description 2.')
+      expect(response.body).to include('https://example.com/resource2')
     end
   end
 
