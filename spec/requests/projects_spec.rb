04426fbdf5ad3f83831bb0ba0ec309df479db0f0
Brittany Halterman <halterma@gmail.com>
2026-01-14 11:23:16 -0500

    Refactor request specs for DRYness

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,34 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :request do
-  describe "GET /projects" do
-    let!(:user) do
+  let!(:user1) do
+    User.create!(
+      email: 'user@example.com',
+      password: 'secret'
+    )
+  end
+
+    let!(:user2) do
       User.create!(
-        email: 'user@example.com',
+        email: 'user2@example.com',
         password: 'secret'
       )
     end
 
-    let!(:project1) do
-      Project.create!(
-        title: 'Test Project 1',
-        description: 'Test project description 1.',
-        user: user
-      )
-    end
+  let!(:project1) do
+    Project.create!(
+      title: 'Test Project 1',
+      description: 'Test project description one.',
+      user: user1
+    )
+  end
 
-    let!(:project2) do
-      Project.create!(
-        title: 'Test Project 2',
-        description: 'Test project description 2.',
-        user: user
-      )
-    end
+  let!(:project2) do
+    Project.create!(
+      title: 'Test Project 2',
+      description: 'Test project description two.',
+      user: user1
+    )
+  end
 
-    before do
-      sign_in user, scope: :user
-    end
+  let!(:project3) do
+    Project.create!(
+      title: 'New Project 2',
+      description: 'New project description three.',
+      user: user2
+    )
+  end
 
+  before do
+    sign_in user1, scope: :user
+  end
+
+  describe "GET /projects" do
     it 'responds with 200 OK' do
       get '/projects'
       expect(response).to have_http_status(:ok)
@@ -37,63 +52,33 @@ RSpec.describe "Projects", type: :request do
     it 'returns a page containing title and descriptions of all the projects' do
       get '/projects'
       expect(response.body).to include('Test Project 1')
-      expect(response.body).to include('Test project description 1.')
+      expect(response.body).to include('Test project description one.')
       expect(response.body).to include('Test Project 2')
-      expect(response.body).to include('Test project description 2.')
+      expect(response.body).to include('Test project description two.')
     end
   end
 
   describe 'GET /projects/:id' do
-    let!(:user) do
-      User.create!(
-        email: 'user@example.com',
-        password: 'secret'
-      )
-    end
-
-    let!(:project) do
-      Project.create!(
-        title: 'Test Project',
-        description: 'Test project description.',
-        user: user
-      )
-    end
-
-    before do
-      sign_in user, scope: :user
-    end
-
     it 'responds with 200 OK' do
-      get "/projects/#{project.id}"
+      get "/projects/#{project1.id}"
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'returns a page containing the project title' do
-      get "/projects/#{project.id}"
+      get "/projects/#{project1.id}"
 
-      expect(response.body).to include('Test Project')
+      expect(response.body).to include('Test Project 1')
     end
 
     it 'returns a page containing the project description' do
-      get "/projects/#{project.id}"
+      get "/projects/#{project1.id}"
 
-      expect(response.body).to include('Test project description')
+      expect(response.body).to include('Test project description one.')
     end
   end
 
   describe 'GET /projects/new' do
-    let!(:user) do
-      User.create!(
-        email: 'user@example.com',
-        password: 'secret'
-      )
-    end
-
-    before do
-      sign_in user, scope: :user
-    end
-
     it 'displays title and description labels' do
       get '/projects/new'
 
@@ -103,17 +88,6 @@ RSpec.describe "Projects", type: :request do
   end
 
   describe 'POST /projects' do
-    let!(:user) do
-      User.create!(
-        email: 'user@example.com',
-        password: 'secret'
-      )
-    end
-
-    before do
-      sign_in user, scope: :user
-    end
-
     it 'creates a new project associated with the current user when title and description exist' do
       post '/projects', params: {
         project: {
@@ -127,7 +101,7 @@ RSpec.describe "Projects", type: :request do
       expect(Project.last.title).to eq('New Project')
       expect(Project.last.description).to eq('Project description.')
 
-      expect(Project.last.user).to eq(user)
+      expect(Project.last.user).to eq(user1)
     end
 
     it 'does not create a project when no title is provided' do
@@ -148,45 +122,11 @@ RSpec.describe "Projects", type: :request do
           description: 'Project description.'
         }
       }
-      expect(response).to have_http_status(:unprocessable_content)
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 
   describe 'GET /projects/:id/edit' do
-    let!(:user1) do
-      User.create!(
-        email: 'user1@example.com',
-        password: 'secret'
-      )
-    end
-
-    let!(:user2) do
-      User.create!(
-        email: 'user2@example.com',
-        password: 'secret'
-      )
-    end
-
-    let!(:project1) do
-      Project.create!(
-        title: 'New Project 1',
-        description: 'New project description one.',
-        user: user1
-      )
-    end
-
-    let!(:project2) do
-      Project.create!(
-        title: 'New Project 2',
-        description: 'New project description two.',
-        user: user2
-      )
-    end
-
-    before do
-      sign_in user1, scope: :user
-    end
-
     it 'responds with 200 OK' do
       get "/projects/#{project1.id}/edit"
 
@@ -206,7 +146,7 @@ RSpec.describe "Projects", type: :request do
     end
 
     it "does not allow access to another user's resource" do
-      get "/projects/#{project2.id}/edit"
+      get "/projects/#{project3.id}/edit"
 
       expect(response).to have_http_status(:not_found)
     end
