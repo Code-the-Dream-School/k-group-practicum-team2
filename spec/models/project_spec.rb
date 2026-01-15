702ab24b216a67570b231982983d78ca76296917
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-15 15:09:20 -0500

    apply copiot changes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe Project, type: :model do
   describe "associations" do
     it "belongs to a user" do
-      user = User.create!(email: "user@test.com", password: "password")
+      user = User.create!(email: "user@email.com", password: "password")
       project = Project.create!(title: "Test Project", user: user)
 
       expect(project.user).to eq(user)
     end
 
     it "can have many skills through project_skills" do
-      user = User.create!(email: "user@test.com", password: "password")
+      user = User.create!(email: "user@email.com", password: "password")
       project = Project.create!(title: "Test Project", user: user)
 
       skill1 = Skill.create!(name: "Rails")
@@ -23,8 +23,8 @@ RSpec.describe Project, type: :model do
     end
 
     it "tracks users who bookmarked the project" do
-      owner = User.create!(email: "owner@test.com", password: "password")
-      favoriter = User.create!(email: "fan@test.com", password: "password")
+      owner = User.create!(email: "owner@email.com", password: "password")
+      favoriter = User.create!(email: "fan@email.com", password: "password")
 
       project = Project.create!(title: "Test Project", user: owner)
       BookmarkedProject.create!(user: favoriter, project: project)
@@ -33,7 +33,7 @@ RSpec.describe Project, type: :model do
     end
 
     it "destroys associated project_skills when deleted" do
-      user = User.create!(email: "user@test.com", password: "password")
+      user = User.create!(email: "user@email.com", password: "password")
       project = Project.create!(title: "Test Project", user: user)
       skill = Skill.create!(name: "Ruby")
 
@@ -45,11 +45,31 @@ RSpec.describe Project, type: :model do
 
   describe "validations" do
     it "is invalid without a title" do
-      user = User.create!(email: "user@test.com", password: "password")
+      user = User.create!(email: "user@email.com", password: "password")
       project = Project.new(title: nil, user: user)
 
       expect(project.valid?).to be false
       expect(project.errors[:title]).to include("can't be blank")
     end
   end
+  describe "validations" do
+    let(:user) { User.create!(email: "user@email.com", password: "password") }
+
+    it "is invalid without a title (nil)" do
+      project = Project.new(title: nil, user: user)
+
+      expect(project.valid?).to be false
+      expect(project.errors[:title]).to include("can't be blank")
+    end
+
+    it "is invalid with an empty title ('')" do
+      project = Project.new(title: "", user: user)
+
+      expect(project.valid?).to be false
+      expect(project.errors[:title]).to include("can't be blank")
+    end
+
+  
+  end
+
 end
