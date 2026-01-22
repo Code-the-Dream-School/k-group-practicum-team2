3aa684805e9874e730b1fd221fa3b9e8f03f9974
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-22 09:14:18 -0500

    Migration add skill column to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_15_153351) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_22_141300) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_15_153351) do
     t.datetime "created_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "skill_level"
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
@@ -124,8 +125,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_15_153351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarked_projects", "projects"
   add_foreign_key "bookmarked_projects", "users"
   add_foreign_key "bookmarked_resources", "resources"
