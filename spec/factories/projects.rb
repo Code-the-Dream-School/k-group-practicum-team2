b6be269f2f6e72316619c609621056d88d09c1b9
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-22 10:29:41 -0500

    Update tests

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :project do
         description { "project description" }
-        tech_stack { "tech stack" }
+
         title { "project title" }
         association :user
     end
