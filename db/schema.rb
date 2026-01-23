782179c37ecf0f1099c15f9663b32800b60e469e
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-23 16:15:50 -0500

    DB migration profile skill_level not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_145649) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_23_211157) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_22_145649) do
     t.datetime "created_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "skill_level"
+    t.string "skill_level", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
