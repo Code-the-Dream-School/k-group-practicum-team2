73fffcff57adfa8d49498ef535e15750f2f3c2fa
abraham <abraham@abrahams-MacBook-Air.local>
2026-01-23 15:51:36 -0500

    Remove any reference to tech stack

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
   has_many :bookmarked_projects, dependent: :destroy
   has_many :favorited_by, through: :bookmarked_projects, source: :user
 
-  # tech stack associations (based on ProjectSkill plan)
+  # project_skills associations (based on ProjectSkill plan)
   has_many :project_skills, dependent: :destroy
   has_many :skills, through: :project_skills
 
