6315e9b9d4b1c7c44f7e64707ab79163cb245e95
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2026-01-27 16:34:31 -0500

    remove helper method from applicant controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,4 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
-
-  helper_method :user_signed_in?, :current_user
-
-  def current_user
-    @current_user ||= warden.authenticate(scope: :user)
-  end
-
-  def user_signed_in?
-    current_user.present?
-  end
 end
