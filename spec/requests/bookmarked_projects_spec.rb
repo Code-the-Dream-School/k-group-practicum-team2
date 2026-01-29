5560d5b6e368364e3ef89f2862f0d48dadbb0660
Brittany Halterman <halterma@gmail.com>
2026-01-29 12:56:18 -0500

    Add a request spec skeleton for BookmarkedProjects

--- spec/requests/bookmarked_projects_spec.rb
+++ spec/requests/bookmarked_projects_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "BookmarkedProjects", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'POST /bookmarked_projects' do
+    it 'creates a new project bookmark'
+    it 'does not create duplicate bookmarks'
+    it 'does not bookmark a project that does not exist'
+  end
+
+  describe 'DELETE /bookmarked_projects' do
+    it 'deletes the project bookmark'
+    it "does not allow a user to remove another user's bookmark"
   end
 end
