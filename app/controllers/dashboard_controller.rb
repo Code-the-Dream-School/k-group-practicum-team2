0b34c28054c54b9efc2b7734daa03ec46086c4de
Brittany Halterman <halterma@gmail.com>
2026-02-01 15:34:53 -0500

    Move project _partial conditional logic into single ERB block and refactor to avoid unnecessary database queries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     @bookmarked_projects = current_user.bookmarked_projects.includes(project: :project_skills).map(&:project)
+    @bookmarked_project_ids = current_user.bookmarked_projects.pluck(:project_id).to_set
   end
 end
