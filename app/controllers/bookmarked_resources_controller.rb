4da2ab72eb3fb6d270ed3f480989e9a5fa99f43f
Brittany Halterman <halterma@gmail.com>
2026-02-02 16:35:35 -0500

    Implement create and delete actions

--- app/controllers/bookmarked_resources_controller.rb
+++ app/controllers/bookmarked_resources_controller.rb
@@ -1,6 +1,25 @@
-class BookmarkedProjectsController < ApplicationController
+class BookmarkedResourcesController < ApplicationController
   before_action :authenticate_user!
 
-  def create; end
-  def destroy; end
+  def create
+    @resource = Resource.find(params[:resource_id])
+    @bookmarked_resource = current_user.bookmarked_resources.new(resource: @resource)
+
+    if @bookmarked_resource.save
+      flash[:notice] = "#{@resource.title} successfully bookmarked!"
+    else
+      flash[:alert] = "#{@resource.title} failed to be saved as a bookmark."
+    end
+
+    redirect_back(fallback_location: resource_path(@resource))
+  end
+
+  def destroy
+    @bookmarked_resource = current_user.bookmarked_resources.find(params[:id])
+    @resource = @bookmarked_resource.resource
+    @bookmarked_resource.destroy
+
+    flash[:notice] = "Resource is no longer bookmarked."
+    redirect_back(fallback_location: resource_path(@resource))
+  end
 end
