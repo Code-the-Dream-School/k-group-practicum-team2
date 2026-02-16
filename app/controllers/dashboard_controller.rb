7be953d6abdc83ce9ea192fcea123842cb4dc047
Brittany Halterman <halterma@gmail.com>
2026-02-16 18:58:59 -0500

    Remove unnused instance variable

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user
-
     bookmarked_projects = current_user.bookmarked_projects.includes(project: { project_skills: :skill })
     @bookmarked_projects = bookmarked_projects.map(&:project)
     @project_bookmark_ids = bookmarked_projects.pluck(:project_id, :id).to_h
